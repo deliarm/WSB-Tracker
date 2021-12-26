4390209364c885ec68439caca3ae8df3130226c6
DeliarM <lioneldeliar520@gmail.com>
2021-12-25 19:10:01 -0700

    working with tickers only
    first commit with tickers working. Will have to implement company names

--- WallStreetBets.py
+++ WallStreetBets.py
@@ -2,20 +2,21 @@ import datetime
 import re
 import warnings
 import time
-import seaborn
 import matplotlib as plt
 import yfinance as yf
 from psaw import PushshiftAPI
-import config
 import psycopg2
 import psycopg2.extras
+from tickers import ticker_set
+warnings.filterwarnings("ignore")
 
 
 year = 2021
-month = 12
-day = 22
-iterations = 8000  # 10k seems to be a good benchmark
-top = 20
+month = 6
+day = 6
+iterations = 10000  # 10k seems to be a good benchmark
+top = 100
+stock_dictionary = {}
 
 
 def has_numbers(inputString):
@@ -23,63 +24,64 @@ def has_numbers(inputString):
 
 
 def valid_stock(inputString):
-    chars = set('.,()#%^*!@?<>/\-+&')
-    if any((c in chars) for c in inputString):
-        return False
-    else:
+    if inputString.upper() in ticker_set:
         return True
+    return False
 
 
 def get_change(current, previous):
     return ((current - previous) / previous) * 100.0
 
 
-warnings.filterwarnings("ignore")
-stock_dictionary = {}
-print("\nFetching data from "+str(year)+"-"+str(month)+"-"+str(day) + "...\n")
+def filler_word_filter(str):
+    if len(str) > 5:
+        return True
+    if has_numbers(str):
+        return True
+    return False
 
+
+### BEGIN PROGRAM ###
+print("\nFetching data from "+str(year)+"-"+str(month)+"-"+str(day) + "...\n")
 
 api = PushshiftAPI()
 date = int(datetime.datetime(year, month, day).timestamp())
 
-# generator
+# API call for posts
 submissions = list(api.search_submissions(after=date,
                                           subreddit='wallstreetbets',
-                                          filter=['url', 'author',
-                                                  'title', 'subreddit'],
+                                          filter=['title'],
                                           limit=iterations))
+
+# iterate through posts and get stock mentions
 for i in submissions:
     print(i)
     words = i.title.split()
-    cashtag = list(
-        set(filter(lambda word: word.lower().startswith('$'), words)))
-    for tag in cashtag:
-        # print(tag)   # optional print statment to see raw data
-        if (len(tag) >= 2 and len(tag) <= 5):
-            if (has_numbers(tag) == False):
-                if(valid_stock(tag) == True):
-                    if(tag.upper() in stock_dictionary.keys()):
-                        stock_dictionary[tag.upper()] += 1
-                    else:
-                        stock_dictionary[tag.upper()] = 1
+    for word in words:
+        if(valid_stock(word)):
+            if (word.upper() in stock_dictionary):
+                stock_dictionary[word.upper()] += 1
+            else:
+                stock_dictionary[word.upper()] = 1
+
 
 # sort and get only top amount declared
 sorted_stocks = sorted(stock_dictionary.items(),
                        key=lambda x: x[1], reverse=True)[:top]
+
 print("\n Sorted Data: ")
-# print stocks and times mentioned
 for stock, count in sorted_stocks:
     print(stock, " \t: ", count)
 print("\n")
 
 
 # display stock data:
-print("Stock \t Volume \t Mentions")
-for stock, count in sorted_stocks:
-    try:
-        temp = yf.Ticker(stock[1:])
-        day_volume = (temp.info["volume"])
-        print(str(stock[1:]) + "\t" + str(day_volume)+" \t   "+str(count))
-    except:
-        print("Stock info not available for "+str(stock[1:]))
-print("\n")
+# print("Stock \t Volume \t Mentions")
+# for stock, count in sorted_stocks:
+#     try:
+#         temp = yf.Ticker(stock[1:])
+#         day_volume = (temp.info["volume"])
+#         print(str(stock[1:]) + "\t" + str(day_volume)+" \t   "+str(count))
+#     except:
+#         print("Stock info not available for "+str(stock[1:]))
+# print("\n")
